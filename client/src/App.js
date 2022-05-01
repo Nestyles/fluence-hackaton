4d6ad5fa8f801998960fe30abcdbc94e2b0f0ba9
Nestyles <tom.sancho@epitech.eu>
2022-05-01 16:30:28 +0200

    [ADD] Very basic chat with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  function setNewMessage(msg) {
+    setMessages([
+      ...messages,
+      msg
+    ]);
+  }
+  function sendMessage(user) {
+    user.preventDefault();
+    const msg = {
+      username: user.target.username.value,
+      text: user.target.text.value
+    };
+    setNewMessage(msg)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-title">P2P Chatapp with Fluence :)</div>
+                <hr/>
+                <div className="messages">
+                  {messages.map(msg => {
+                    return (
+                        <div className="key">{msg.username}: {msg.text}</div>
+                    )
+                  })}
+                </div>
+              </div>
+              <form onSubmit={e => sendMessage(e)}>
+                <div className="card-footer">
+                  <input id="username"
+                         type="text"
+                         placeholder="Username"
+                         className="form-control"
+                  />
+                  <br/>
+                  <input id="text"
+                         type="text"
+                         placeholder="Your message"
+                         className="form-control"
+                  />
+                  <br/>
+                  <button type="submit"
+                          className="btn btn-primary form-control">
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
